5e78f5f9fc41f2303b3ab9969bc5cde06492974d
CemMusluoglu <33870889+CemMusluoglu@users.noreply.github.com>
2018-05-07 16:19:47 +0200

    mapping

--- transmitter.m
+++ transmitter.m
@@ -5,7 +5,11 @@ close all
 file=fopen('test.txt');
 text_cell=textscan(file,'%c');
 text=text_cell{1};
-binary_seq=str2num(reshape(dec2bin(text)',[],1));
+binary_array=dec2bin(text);
+if(size(binary_array,2)==7)
+    binary_array=strcat('0',binary_array);
+end
+binary_seq=str2num(reshape(binary_array',[],1));
 
 codewords=mapping(binary_seq);
 
