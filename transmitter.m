574319f615829d530e6b1c634e7cc4927abe3473
wodw0 <nawel.naas@gmail.com>
2018-05-17 14:47:11 +0200

    transmitter and waveformer

--- transmitter.m
+++ transmitter.m
@@ -2,14 +2,20 @@ clear all
 close all
 
 % Read file and convert to binary ASCII
-file=fopen('test.txt');
-text_cell=textscan(file,'%c');
-text=text_cell{1};
-binary_array=dec2bin(text);
-if(size(binary_array,2)==7)
-    binary_array=strcat('0',binary_array);
+file = fopen('test.txt');
+text_cell = textscan(file,'%c');
+text = text_cell{1};
+binary_array = dec2bin(text);
+if(size(binary_array,2) == 7)
+    binary_array = strcat('0',binary_array);
 end
-binary_seq=str2num(reshape(binary_array',[],1));
+binary_seq = str2num(reshape(binary_array',[],1));
+codewords = mapping(binary_seq);
 
-codewords=mapping(binary_seq);
+rolloff = 0.25;
+span = 600;
+sps = 40;
+wave = waveformer(codewords, rolloff, span, sps);
+
+sound(wave, sps*span);
 
--- waveformer.m
+++ waveformer.m
@@ -1,36 +1,27 @@
-clear all
-rolloff = 0.25;
-span = 800;
-sps = 8;
+function wave = waveformer(codeword, rolloff, span, sps)
+%WAVEFORMER Summary of this function goes here
+%   Detailed explanation goes here
 
 B = rcosdesign(rolloff, span, sps);
-h = impz(B,1);
-H = fft(h);
-bw = length(H);
-plot([(-bw+1)/2:1:(bw-1)/2],abs(fftshift(H)))
-
 fs = span*sps;
-t = [0:1/fs:1];
+t = 0:1/fs:1;
+tstart = find(B>1e-3,1,'first');
+tend = find(B>1e-3,1,'last');
+B_trunc = B(tstart:tend);
 
 f1 = 1500; % Hz
-x = sin(2*pi*f1*t);
-h1 = x'.*h;
-H1 = fft(h1);
-hold on
-plot([(-bw+1)/2:1:(bw-1)/2],abs(fftshift(H1)))
-
-player = audioplayer(10*h1,fs);
-playblocking(player)
+x1 = cos(2*pi*f1*t(1:length(B_trunc)));
+y1 = sin(2*pi*f1*t(1:length(B_trunc)));
 
 f2 = 2500; % Hz
-y = sin(2*pi*f2*t);
-h2 = y'.*h;
-H2 = fft(h2);
-hold on
-plot([(-bw+1)/2:1:(bw-1)/2],abs(fftshift(H2)))
+x2 = cos(2*pi*f2*t(1:length(B_trunc)));
+y2 = sin(2*pi*f2*t(1:length(B_trunc)));
 
-player = audioplayer(10*h2,fs);
-playblocking(player)
+A = real(codeword)*(x1+x2) + imag(codeword)*(y1+y2);
 
+wave = A.*B_trunc;
+wave = wave';
+wave = wave(:);
 
+end
 
