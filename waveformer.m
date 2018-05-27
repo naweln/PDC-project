4e77f2a0f6f8bfa03f7aa1509b135aa60fdcf88a
wodw0 <nawel.naas@gmail.com>
2018-05-28 01:45:46 +0200

    waveformer looks nicer

--- waveformer.m
+++ waveformer.m
@@ -1,22 +1,26 @@
-function wave = waveformer(codeword, rolloff, span, sps)
+function wave = waveformer(codeword, rolloff, span, sps, threshold)
 %WAVEFORMER Summary of this function goes here
 %   Detailed explanation goes here
 
-threshold=1e-3;
 B = rcosdesign(rolloff, span, sps);
 fs = 2*span*sps;
-t = 0:1/fs:10;
-tstart = find(B>threshold, 1,'first');
-tend = find(B>threshold, 1,'last');
-B_trunc = B(tstart:tend);
+t = 0:1/fs:10000;
+if threshold
+    tstart = find(B>threshold, 1,'first');
+    tend = find(B>threshold, 1,'last');
+    B_trunc = B(tstart:tend);
+else
+    B_trunc = B;
+end
 
 f1 = 1500; % Hz
 f2 = 2500; % Hz
-T=500;
 
-signal=convolve(upsample(codeword,T),B_trunc.');
-wave=real(signal).*cos(2*pi*f1*t(1:length(signal)))'-imag(signal).*sin(2*pi*f1*t(1:length(signal)))';
-wave=wave+real(signal).*cos(2*pi*f2*t(1:length(signal)))'-imag(signal).*sin(2*pi*f2*t(1:length(signal)))';
+signal= convolve(upsample(codeword,length(B_trunc)),B_trunc.'); % upsampling by B_trunc eliminates ISI
+harm1 = exp(-2*pi*1i*f1*t(1:length(signal)));
+harm2 = exp(-2*pi*1i*f2*t(1:length(signal)));
+
+wave = sqrt(2)*real(signal.*(harm1+harm2)');
 
 end
 
