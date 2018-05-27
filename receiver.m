e7da36bd8e36b6141c58439034acd47d38f3a0a8
wodw0 <nawel.naas@gmail.com>
2018-05-28 01:45:18 +0200

    made receiver into a function and moved the script from transmitter to a different file

--- receiver.m
+++ receiver.m
@@ -1,19 +1,48 @@
-clear all
-close all
-
-recorder = audiorecorder(44100,16,1); 
-recordblocking(recorder, 5);
-y = getaudiodata(recorder);
-
-rolloff = 0.25;
-span = 300;
-sps = 40;
+function decoded = receiver(rolloff,span,sps,threshold,fs)
 
 B = rcosdesign(rolloff, span, sps);
-fs = 2*span*sps;
-t = 0:1/fs:1;
-tstart = find(B>1e-3,1,'first');
-tend = find(B>1e-3,1,'last');
-B_trunc = B(tstart:tend+~mod(tend-tstart,2));
+if threshold>0
+    tstart = find(B>threshold,1,'first');
+    tend = find(B>threshold,1,'last');
+    B_trunc = B(tstart:tend);
+else
+    B_trunc = B;
+end
+T = length(B_trunc);
 
-y_matched=convolve(y,B_trunc.');
+% recording
+% recorder = audiorecorder(fs,16,1);
+% recordblocking(recorder, 5);
+% y = getaudiodata(recorder);
+
+% synchronization - todo.
+
+% test (remove todo)
+
+y = transmitter(rolloff,span,sps,threshold);
+n = noise(fs);
+y = y + n(1:length(y));
+
+% detecting frequency band
+Yf = abs(fft(y));
+f = linspace(0,fs,length(y));
+indices1 = find(f>1200 & f<1800);
+indices2 = find(f>2200 & f<2800);
+if max(Yf(indices2)>max(Yf(indices1)))
+    fc = 1500;
+else
+    fc = 2500;
+end
+
+t = 0:1/fs:10000;
+y_base = sqrt(2)*y.*exp(-2*pi*1i*fc.*t(1:length(y))).';
+
+y_matched=convolve(y_base,B_trunc.');
+
+decoded_bin=dec2bin(demapping(y_matched(T:T:length(y)+1-T)));
+decoded_bin=reshape(decoded_bin',[],1);
+decoded=reshape(decoded_bin,[],8)';
+decoded=char(bin2dec(decoded))';
+
+
+
---
+++ receiver_script.m
@@ -0,0 +1,56 @@
+clear all
+rolloff = 0.25;
+span = 300;
+sps = 40;
+threshold = 1e-3;
+
+B = rcosdesign(rolloff, span, sps);
+fs = 2*span*sps;
+t = 0:1/fs:100;
+if threshold>0
+    tstart = find(B>threshold,1,'first');
+    tend = find(B>threshold,1,'last');
+    B_trunc = B(tstart:tend);
+else
+    B_trunc = B;
+end
+
+y = transmitter(rolloff, span, sps, threshold);
+
+
+T = length(B_trunc);
+
+nb_characters=8;
+nb_bits=8*nb_characters;
+nb_codewords=nb_bits/2;
+conv_length=T*nb_codewords+length(B_trunc)-1;
+
+%filt1=filters(800,2200,fs,'bandpass');
+%y1=filter(filt1,y);
+y1=sqrt(2)*y.*exp(-2*pi*1i*1500.*t(1:conv_length)).';
+%y1_passband=2*lowpass(y1,1500,fs);
+% 
+% %filt2=filters(1800,3200,fs,'bandpass');
+% %y2=filter(filt2,y);
+y2=sqrt(2)*y.*exp(-2*pi*1i*2500.*t(1:conv_length)).';
+%y2_passband=2*lowpass(y2,2500,fs);
+
+%plot(abs(fft(y1_passband)))
+%figure
+%plot(abs(fft(y2_passband)))
+
+y_matched1=convolve(y1,B_trunc.');
+y_matched2=convolve(y2,B_trunc.');
+
+decoded1_bin=dec2bin(demapping(y_matched1(T:T:T*nb_codewords)));
+decoded1_bin=reshape(decoded1_bin',[],1);
+decoded1=reshape(decoded1_bin,[],8)';
+decoded1=char(bin2dec(decoded1))';
+
+decoded2_bin=dec2bin(demapping(y_matched2(T:T:T*nb_codewords)));
+decoded2_bin=reshape(decoded2_bin',[],1);
+decoded2=reshape(decoded2_bin,[],8)';
+decoded2=char(bin2dec(decoded2))';
+
+% constellation
+plot(y_matched1(T:T:T*nb_codewords),'o')
